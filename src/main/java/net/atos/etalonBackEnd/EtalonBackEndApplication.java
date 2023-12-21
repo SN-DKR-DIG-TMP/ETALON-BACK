ececd6867fe3a53ed152131d686fc3a9b3d5f8da
Abdou MBENGUE <abdou.mbengue@atos.net>
2023-12-21 16:13:26 +0000

    first commit change

---
+++ src/main/java/net/atos/etalonBackEnd/EtalonBackEndApplication.java
@@ -0,0 +1,13 @@
+package net.atos.etalonBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtalonBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtalonBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/atos/etalonBackEnd/EtalonBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package net.atos.etalonBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtalonBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
